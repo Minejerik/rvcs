1c47f1dc296c97e547a3e63101e05f4d63f19144
minejerik <jaanerikf@gmail.com>
2023-10-03 11:00:31 -0400

    working on RVCS

--- src/main.rs
+++ src/main.rs
@@ -2,17 +2,37 @@ use std::env;
 use std::path::Path;
 use std::fs;
 
+fn get_current_working_dir() -> String {
+    let res = env::current_dir();
+    match res {
+        Ok(path) => path.into_os_string().into_string().unwrap(),
+        Err(_) => "FAILED".to_string()
+    }
+}
 
 
 fn new() {
-    if Path::new("/.rvcs").exists() {
+
+
+    let cur_path = get_current_working_dir();
+
+    let new_path = format!("{}/.rvcs", cur_path);
+
+
+
+    if Path::new(&new_path).exists() {
         println!("RVCS aleady exists");
         return;
     }
 
-    let _ = fs::create_dir("/.rvcs");
+    let temp = fs::create_dir(new_path);
 
-    
+    match temp {
+        Ok(_) => println!("RVCS Initialized"),
+        Err(_) => println!("Error creating RVCS")
+    }
+
+
 
 
 }
