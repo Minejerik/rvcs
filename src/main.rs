b2bbe0ddd5ba0d9d6d4c0914b2716240647e3f68
minejerik <jaanerikf@gmail.com>
2023-10-03 10:53:16 -0400

    working on new trolling

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
+use std::env;
+use std::path::Path;
+use std::fs;
+
+
+
+fn new() {
+    if Path::new("/.rvcs").exists() {
+        println!("RVCS aleady exists");
+        return;
+    }
+
+    let _ = fs::create_dir("/.rvcs");
+
+    
+
+
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    if &args[1] == "new" {
+        new();
+    } else if &args[1] == "commit" {
+        println!("build");
+    }
+
+    println!("args: {:?}", args);
 }
